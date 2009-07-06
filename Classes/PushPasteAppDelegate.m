442e82882b52ab67ca647f12ee046973c9cd2a29
mootoh <mootoh@gmail.com>
2009-07-07 01:01:10 +0900

    described initial flow.

--- Classes/PushPasteAppDelegate.m
+++ Classes/PushPasteAppDelegate.m
@@ -14,20 +14,45 @@
 @synthesize window;
 @synthesize viewController;
 
-
-- (void)applicationDidFinishLaunching:(UIApplication *)application {    
-    
-    // Override point for customization after app launch    
-    [window addSubview:viewController.view];
-    [window makeKeyAndVisible];
+- (void) loadSetting
+{
+   
 }
 
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+   // if launched from APN
+   //   show preview
+   //   ask what app to kick
+   //   kick that app or exit
+   //
+   // load setting
+   // if initial execution
+   //   show welcome screen
+   //   ask password if not exist
+   //   save password
+   //   search for Mac app via Bonjour
+   //   show guidance to launch Mac side app
+   //   wait for Mac app to start
+   //   recieve password from Mac app
+   //   verify password
+   //   send {ok:UUID or ng} to Mac app
+   //   waiting for response
+   //   show "OK" if recieved success
+   //   put down initial flag
+   //
+   // regist to APN
+   // [[UIApplication sharedApplication] registerForRemoteNotificationTypes:(UIRemoteNotificationTypeBadge | UIRemoteNotificationTypeSound | UIRemoteNotificationTypeAlert)];
 
-- (void)dealloc {
-    [viewController release];
-    [window release];
-    [super dealloc];
+   [window addSubview:viewController.view];
+   [window makeKeyAndVisible];
 }
 
+- (void)dealloc
+{
+   [viewController release];
+   [window release];
+   [super dealloc];
+}
 
 @end
