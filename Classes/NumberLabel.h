0c6e708b24729c85d4c5021faad15fa0615a8257
takayama@MacBookAir <motohiro.takayama@toshiba.co.jp>
2009-07-08 10:37:15 +0900

    use UILabel instead of UIButton as base class.

--- Classes/NumberLabel.h
+++ Classes/NumberLabel.h
@@ -1,18 +1,18 @@
 //
-//  NumberButton.h
+//  NumberLabel.h
 //  PushPaste
 //
 //  Created by mootoh on 7/7/09.
 //  Copyright 2009 deadbeaf.org. All rights reserved.
 //
 
-@interface NumberButton : UIButton
+@interface NumberLabel : UILabel
 {
-   IBOutlet NumberButton *next;
+   IBOutlet NumberLabel *next;
    NSString *number;
 }
 
-@property (nonatomic, assign) NumberButton *next;
+@property (nonatomic, assign) NumberLabel *next;
 @property (nonatomic, retain) NSString *number;
 
 @end
